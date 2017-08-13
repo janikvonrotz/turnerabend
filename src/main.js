b7a994aace7dcb3e144348b70be15375232ad4d9
Janik von Rotz <login@janikvonrotz.ch>
2017-08-13 16:51:57 +0200

    update active link set

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
+// functions
+var forEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]); 
+  }
+}
 
 // initialize smooth scroll
-var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('a[href*="#"]')
 
 // initialize in view
 inView.offset({
     top: 200,
     bottom: 200,
-});
+})
 inView('section')
 .on('enter', el => {
     if (el.id.substring(0, 7) == "section") {
-        var link = document.querySelectorAll("a[href='#" + el.id + "']")[0];
-        link.classList.add('active');
+        var link = document.querySelectorAll("a[href='#" + el.id + "']")[0]
+        var links = document.querySelectorAll("nav a")
+        forEach(links, (index, link) => {
+            link.classList.remove('active')
+        })
+        link.classList.add('active')
         if(history.pushState) {
-            history.pushState(null, null, '#' + el.id);
+            history.pushState(null, null, '#' + el.id)
         }
-    }
-})
-.on('exit', el => {
-    if (el.id.substring(0, 7) == "section") {
-        var link = document.querySelectorAll("a[href='#" + el.id + "']")[0];
-        link.classList.remove('active');
     }
 })
