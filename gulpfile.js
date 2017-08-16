8a2ece0aed3a94b76ae865d6af033f61345b5317
Janik von Rotz <login@janikvonrotz.ch>
2017-08-16 20:57:21 +0200

    Refactor everything

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
+var rename = require('gulp-rename');
+var merge = require('merge-stream');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 // run browser sync in current directory
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './dist'
     },
   })
 })
@@ -16,7 +20,7 @@ gulp.task('browserSync', function() {
 gulp.task('useref', function(){
   return gulp.src('src/**/*.html')
     .pipe(useref())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./dist'))
     .pipe(browserSync.reload({
       stream: true
     }))
@@ -26,28 +30,52 @@ gulp.task('useref', function(){
 gulp.task('sass', function(){
   return gulp.src('src/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./dist'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('copy', function() {
-  return gulp.src([
-    'node_modules/animate.css/animate.css',
+// copy css
+gulp.task('copy-css', function() {
+  var files = [
     'node_modules/normalize.css/normalize.css'
-  ])
-  .pipe(gulp.dest('./src'))
+  ]
+  var streams = []
+  files.forEach((file) => {
+    var stream = gulp.src(file)
+      .pipe(rename((path) => {
+        path.basename = `_${path.basename}`
+        path.extname = '.scss'
+      }))
+      .pipe(gulp.dest('./src/partials'))
+    streams.push(stream)
+  })
+  return merge(streams)
+})
+
+// copy images
+gulp.task('copy-images', function(){
+  return gulp.src('src/assets/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin({
+    interlaced: true
+    })))
+  .pipe(gulp.dest('./dist/assets'))
+});
+
+// clear gulp cache
+gulp.task('clear', function (done) {
+  return cache.clearAll(done);
+});
+
+// buil gulp project
+gulp.task('build', ['copy-css', 'copy-images', 'sass', 'useref'], () => {
+  console.log('Building files')
 })
 
 // start browser sync and watch directories
 gulp.task('watch', ['browserSync'], function(){
     gulp.watch('src/**/*.scss', ['sass'])
-    gulp.watch('src/**/*.html', ['useref']); 
-    gulp.watch('src/**/*.js', ['useref']); 
-})
-
-// buil gulp project
-gulp.task('build', ['copy', 'sass', 'useref'], function (){
-  console.log('Building files');
+    gulp.watch('src/**/*.html', ['useref'])
+    gulp.watch('src/**/*.js', ['useref'])
 })
